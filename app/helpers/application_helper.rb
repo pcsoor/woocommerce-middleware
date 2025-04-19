5ae737beca63263f01a05e02a1b18e0622ca03ff
Peter Csoor <peter.csoor@gmail.com>
2025-04-19 12:42:59 +0200

    add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def extract_store_name(api_url)
+    return unless api_url.present?
+
+    uri = URI.parse(api_url)
+    domain = uri.host
+
+    domain&.sub(/^www\./, '')
+  rescue URI::InvalidURIError
+    nil
+  end
 end
