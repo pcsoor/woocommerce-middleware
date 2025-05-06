d1b5447d2cbdf1ef55e97408ef6d806c8f0a029c
Peter Csoor <peter.csoor@gmail.com>
2025-05-07 01:17:58 +0200

    add stimulus dropdown component and do basic stlying on nav

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import Dropdown from "@stimulus-components/dropdown"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+application.register('dropdown', Dropdown)
 
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
 eagerLoadControllersFrom("controllers", application)
